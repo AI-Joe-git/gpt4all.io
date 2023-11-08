93058fb5fc4bf856485cb2f88435dd05616b17b4
A.I Joe <88169436+AI-Joe-git@users.noreply.github.com>
2023-11-08 05:51:50 +0100

    Update App.js

--- frontend/gpt4all.io/src/App.js
+++ frontend/gpt4all.io/src/App.js
@@ -81,7 +81,7 @@ function App() {
           <img
             alt="Nomic AI logo"
             src={process.env.PUBLIC_URL + "/favicon.ico"}
-            className="w-12 h-8"
+            className="w-12 h-12"
           />
         </a>
         <title>GPT4All Pro</title>
